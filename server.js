12cdf8a26b017be25c6c59f13fe175d80d38d494
jhon lawrence <caracoljl@gmail.com>
2026-04-14 07:25:43 +0800

    Fix wildcard route syntax

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.post('/api/users', async (req, res) => {
   }
 });
 
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'web/bbpwdo-system/public/index.html'));
 });
 
