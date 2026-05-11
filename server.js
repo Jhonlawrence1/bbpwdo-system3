2e8fd47b98999a0da3e9c27e4be9cd9374da6b6a
jhon lawrence <caracoljl@gmail.com>
2026-05-11 08:28:40 +0800

    Fix CORS and add error display

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.static(path.join(__dirname, 'web/bbpwdo-system/public')));
 
 const PORT = process.env.PORT || 3000;
